f102fe6cd0d4cc6ccb5e491cadebefb3ccccbf63
Leonid Lezner <leonidlezner@users.noreply.github.com>
2022-09-11 21:10:36 +0200

    Update with-tailwind.sh

--- with-tailwind.sh
+++ with-tailwind.sh
@@ -4,39 +4,4 @@ printf "Project name: "
 
 read projectname
 
-
-# npx create-react-app $projectname
-
-# cd $projectname
-
-# npm install -D tailwindcss postcss autoprefixer prettier prettier-plugin-tailwindcss
-
-# npx tailwindcss init -p
-
-# sed -i '' 's/content: \[\]/content: \[ ".\/src\/**\/*.{js,ts,jsx,tsx}" \]/g' tailwind.config.js
-
-# touch .prettierrc.json
-
-# mkdir -p .vscode
-
-# cat >./.vscode/settings.json <<EOT
-# {
-#     "editor.formatOnSave": true,
-#     "editor.defaultFormatter": "esbenp.prettier-vscode"
-# }
-# EOT
-
-# cat >./src/index.css <<EOT
-# @tailwind base;
-# @tailwind components;
-# @tailwind utilities;
-# EOT
-
-# cat >./src/App.js <<EOT
-# export default function App() {
-#   return <div></div>;
-# }
-# EOT
-
-# rm ./src/App.css
-# rm ./src/logo.svg
+echo $projectname
